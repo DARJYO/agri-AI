5f03d8649450ecb991dae1dc9b945358a1c4b3e3
persadian <34051185+arishma108@users.noreply.github.com>
2025-01-20 12:16:51 +0200

    Update BaseChat.tsx

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -329,6 +329,9 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
                 <p className="text-md lg:text-xl mb-8 text-bolt-elements-textSecondary animate-fade-in animation-delay-200">
                   Transform your concepts into reality or get help on active projects.
                 </p>
+                <H6>
+  <p>© 2025 DARJYO. All Rights Reserved.</p>
+</H6>
               </div>
             )}
             <div
